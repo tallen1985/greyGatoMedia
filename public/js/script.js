4749074d51f93c686a30c81e806ec06bfe1a94ee
Troy Allen <jake.allen@me.com>
2021-12-01 14:41:03 -0500

    working model

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,31 @@
-var i = 0;
-var txt = 'Grey Gato Media'; /* The text */
-var speed = 50;
+const articles = document.getElementsByTagName('article');
+const navItems = document.getElementsByClassName('nav-link');
+const contactLink = document.getElementById('contactLink');
+const homeLink = document.getElementById('homeLink')
+const servicesLink = document.getElementById('servicesLink');
+
+let i = 0;
+const txt = 'Grey Gato Media'; /* The text */
+const speed = 50;
+
+for (nav of navItems){
+  nav.addEventListener('click', (e) => {
+    for (article of articles) {
+      article.style.display = 'none';
+    }
+    if (e.target == contactLink) {
+      document.getElementById('contactSection').style.display = 'block';
+    }
+    if (e.target == servicesLink) {
+      document.getElementById('servicesSection').style.display = 'block';
+    }
+    if (e.target == homeLink) {
+      for (article of articles) {
+        article.style.display = 'block';
+      }
+    }
+  })
+}
 
 function typeWriter() {
   if (i < txt.length) {
@@ -9,5 +34,7 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
+
 
 typeWriter();
