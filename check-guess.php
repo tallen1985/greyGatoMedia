958122f906face7d0350b0cd337c3f898a59545a
Troy Allen <jake.allen@me.com>
2022-05-14 18:17:33 -0400

    trying this

--- check-guess.php
+++ check-guess.php
@@ -1,5 +1,19 @@
 <?php 
 include 'databaseConnect.php';
+
+$sql = "SELECT * FROM GUESSES";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
+  ?>
 ?>
 
 <!DOCTYPE html>
